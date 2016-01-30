0604a1c159d59f4b0b57b8448cd9d4c13de08d9b
inDream <indreamhk@gmail.com>
2016-01-30 19:17:55 +0800

    Version 0.16.1.7

--- DownAlbum.user.js
+++ DownAlbum.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Download FB Album mod
 // @author        indream
-// @version       0.16.1.6
+// @version       0.16.1.7
 // @description   Download Facebook & Instagram Album by One Click.
 // @namespace     DownAlbum
 // @updateURL     https://raw.githubusercontent.com/inDream/DownAlbum/master/DownAlbum.meta.js
