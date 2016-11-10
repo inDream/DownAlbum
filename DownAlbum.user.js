1b782b6cbb8c536983c570fda10f64040fe0e1fe
inDream <indreamhk@gmail.com>
2016-11-10 13:43:36 +0800

    Version 0.16.11.1

--- DownAlbum.user.js
+++ DownAlbum.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          DownAlbum
 // @author        indream
-// @version       0.16.10.4
+// @version       0.16.11.1
 // @description   Download Facebook, Instagram, Pinterest, Twitter, Ask.fm, Weibo Album.
 // @namespace     DownAlbum
 // @updateURL     https://raw.githubusercontent.com/inDream/DownAlbum/master/DownAlbum.meta.js
