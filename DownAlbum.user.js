adc43290a14fc4257bee791835e1317779dd2394
inDream <indreamhk@gmail.com>
2019-04-07 12:46:16 +0800

    Fix pinterest button

--- DownAlbum.user.js
+++ DownAlbum.user.js
@@ -154,7 +154,7 @@ var dFAinit = function(){
     }
   }else if(href.indexOf('pinterest') > 0){
     if(!qS('#dfaButton')){
-      t = qS('.boardHeaderWrapper') || qS('h3') ? qS('h3').parentNode : null;
+      t = qS('.boardHeaderWrapper') || qS('h3, h4') ? qS('h3, h4').parentNode : null;
       if (!t) {
         return;
       }
